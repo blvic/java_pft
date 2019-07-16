f0dd8662b79fe9c8bc4a556fe4af9cfcec9d54fc
blvic <blvic@mail.ru>
2019-07-17 00:28:44 +0300

    Заменил двойное умножение на возведение в степень

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -10,6 +10,6 @@ public class Point {
   }
 
   public double distance (Point p1, Point p2){
-    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+    return Math.sqrt(Math.pow(p2.x-p1.x,2) + Math.pow(p2.y-p1.y,2));
   }
 }
