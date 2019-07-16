5f642b423170d0f251d9b0bfe58db2613da38490
blvic <blvic@mail.ru>
2019-07-17 00:24:08 +0300

    Добавил решение домашнего задания

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,12 +8,17 @@ public static void main(String[] args) {
 hello("World");
   hello("user");
   hello("Vasya");
-  double l =25;
-  double a =4;
-  double b =6;
-  System.out.println("Площадь квадрата = " + "со стороной " + l + " равна "+ area(l));
+  Square s = new Square(5);
+  Rectangle r = new Rectangle(8,4);
 
-  System.out.println("Площадь прямоугольника = " + "со сторонами " + a + " и "+ b + " равна "+ area(a,b));
+  Point p1 = new Point(2,4);
+  Point p2 = new Point(4,6);
+
+  System.out.println("Расстояние между точками = " + "p1 x(" + p1.x + ") y("+p1.y+ ") "+ " и "+ "p2 x(" + p2.x + ") y("+p2.y+ ") равно " + p1.distance(p2,p1) );
+
+  System.out.println("Площадь квадрата = " + "со стороной " + s.l + " равна "+ s.area());
+
+  System.out.println("Площадь прямоугольника = " + "со сторонами " + r.a + " и "+ r.b + " равна "+ r.area());
 }
 
 public static void hello(String somebody) {
@@ -21,11 +26,6 @@ public static void hello(String somebody) {
   System.out.println("Hello "   + somebody +"!");
 }
 
-public static double area (double len){
- return len * len;
-}
 
-public static double area (double a, double b) {
-  return a * b;
-}
+
 }
