88c7eef9002c0ac10a57f7be49420974c9366eca
blvic <blvic@mail.ru>
2019-07-28 23:42:48 +0300

    Добавлен класс для удаления тестовых групп и проведен рефакторинг кода

---
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,66 @@
+package ru.stqua.pft.addressbook;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.fail;
+
+public class GroupDeletionTests extends TestBase {
+
+
+
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+   gotoGroupPage();
+    selectGroup();
+    deleteGroup();
+   gotoGroupPage();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
