29f8a3d5453b691dc0791614257667da73bc266a
blvic <blvic@mail.ru>
2019-07-28 16:58:38 +0300

    Инициализация и завершение автотеста вынесены в отдельные вспомогательные методы @BeforeClass и @AfterClass

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/GroupCreationTests.java
@@ -20,24 +20,24 @@ public class GroupCreationTests {
 
 
     driver = new FirefoxDriver();
-    baseUrl = "https://www.katalon.com/";
+  //  baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testUntitledTestCase() throws Exception {
     driver.get("http://localhost/addressbook/group.php");
-    driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    driver.findElement(By.linkText("groups")).click();
-  
+     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+   // driver.findElement(By.xpath("(.//form[@id='LoginForm']/input[3]")).click();
 
-    driver.findElement(By.linkText("groups")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
+
+   driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
@@ -60,6 +60,8 @@ public class GroupCreationTests {
 
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
+    driver.findElement(By.linkText("groups")).click();
+
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
