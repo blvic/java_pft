2510035c1b964b48a8b748d72504af46bf8d1bb1
blvic <blvic@mail.ru>
2019-07-31 23:14:30 +0300

    Все заработало. После восстановления из GitHub надо делать обновление задач в Gradle

---
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package ru.stqua.pft.addressbook;
+
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+  @Test
+  public void testGroupDeletion() throws Exception {
+   gotoGroupPage();
+    selectGroup();
+    deleteGroup();
+    returnToGroupPage();
+  }
+
+}
