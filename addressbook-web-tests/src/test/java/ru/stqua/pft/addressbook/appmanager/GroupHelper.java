cdfb5d4c6620b40046a70fd7de12460ff531e42a
blvic <blvic@mail.ru>
2020-05-12 17:16:47 +0300

    Добавлна проверка предусловий при удалении и модификации групп

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/appmanager/GroupHelper.java
@@ -48,4 +48,15 @@ public class GroupHelper extends  HelperBase {
   public void initContactModification() {
 
   }
+
+  public void createGroup(GroupData group ) {
+    initGroupCreation();
+     fillGroupForm(group);
+     submitGroupCreation();
+     returnToGroupPage();
+       }
+
+  public boolean isThereAGroup() {
+   return isElementPresent(By.name("selected[]"));
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/GroupDeletionTests.java
@@ -2,11 +2,15 @@ package ru.stqua.pft.addressbook.tests;
 
 
 import org.testng.annotations.Test;
+import ru.stqua.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
    app.getNavigationHelper().gotoGroupPage();
+   if (! app.getGroupHelper().isThereAGroup()) {
+     app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+   }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteGroup();
     app.getGroupHelper().returnToGroupPage();
